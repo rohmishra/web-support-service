69532c2e1b6753ae71ff164ef24f134d0af466e7
Rohan M <rohmish26@gmail.com>
2020-01-31 16:02:26 -0500

    Added route: /

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const express = require("express");
 const App = express();
 const port = 6060;
 
-App.listen(port, (e)=>{
+App.get('/', (req, res) => {
+    res.send(`Working.`);
+})
+
+App.listen(port, (e) => {
     console.log(`listening on ${port}`);
 })
