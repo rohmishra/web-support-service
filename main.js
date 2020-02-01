24552a7759552991869b40524991f4ccb07af2a2
Rohan M <rohmish26@gmail.com>
2020-02-01 01:26:24 -0500

    Added mongo URI to env. Made serve port dynamic from env.

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const express = require("express");
+// Load env vars
+require('dotenv').config();
+
+const express = require('express');
 const App = express();
-const port = 6060;
+
+const serve_port = process.env.PORT || process.env.TEST_PORT;
+const mongo_URI = process.env.MONGODB_URI || process.env.DB_HOST;
 
 App.get('/', (req, res) => {
-    res.send(`Working.`);
-})
+	res.send(`Working.`);
+});
 
-App.listen(port, (e) => {
-    console.log(`listening on ${port}`);
-})
+// App.use('/');
+
+App.listen(serve_port, (e) => {
+	console.log(`listening on ${serve_port}. Connecting to DB at ${mongo_URI}`);
+});
