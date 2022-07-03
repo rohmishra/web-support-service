3afb7e7ecfa32ad7c745f1a5914792f52dce50db
Rohan M <rohan@crazydeveloper.fail>
2022-07-03 14:23:26 -0400

    misc: Cleanuup code
    Signed-off-by: Rohan M <rohan@crazydeveloper.fail>

--- models/ConsultUser.js
+++ models/ConsultUser.js
@@ -1,33 +1,61 @@
 const Mongoose = require('mongoose');
 const userSchema = Mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true,
-		lowercase: true
-	},
-	verification: {
-		random_key: String,
-		isVerified: {
-			type: Boolean,
-			default: false
-		}
-	},
-	last_updated: {
-		type: Date,
-		default: Date.now
-	},
-	created_date: {
-		type: Date,
-		default: Date.now,
-		immutable: true
-	},
-	reff_by: Mongoose.SchemaTypes.ObjectId
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  verification: {
+    random_key: String,
+    isVerified: {
+      type: Boolean,
+      default: false
+    }
+  },
+  last_updated: {
+    type: Date,
+    default: Date.now
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+    immutable: true
+  },
+  reff_by: Mongoose.SchemaTypes.ObjectId
 });
 
+// userSchema.method('verify', (User, cb) => {
+// TODO: Send email verification
+// const DOMAIN = process.env.DOMAIN;
+// const mg = mailgun({
+// 	apiKey: process.env.MG_API_KEY,
+// 	domain: DOMAIN
+// });
+// const data = {
+// 	from: process.env.FROM_TEXT || 'email verification',
+// 	to: User.email,
+// 	subject: `Hi, ${User.name} Lets Talk!`,
+// 	template: 'contact_verification',
+// 	'h:X-Mailgun-Variables': {
+// 		email_base64: Buffer.from(User.email, 'utf8').toString('base64'),
+// 		unique_token: this.verification_token
+// 	}
+// };
+// try {
+// 	mg.messages().send(data, function (error, body) {
+// 		if (envmode === 'DEV') {
+// 			console.log(body);
+// 		}
+// 	});
+// } catch (e) {
+// 	console.message('=== Couldn\'t send email ===');
+// 	console.log(e);
+// }
+// });
 
 
 module.exports = Mongoose.model('User', userSchema);
--- routes/WebActions.js
+++ routes/WebActions.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const userModel = require('../models/ConsultUser.js');
 const cors = require('cors');
-// const mailgun = require('mailgun.js'); // TODO: require mailgun and send email to verify user email
+// const mailgun = require('mailgun-js'); // TODO: require mailgun and send email to verify user email
 var router = express.Router();
 
 // DEV MODE LOGGING
 const envmode = process.env.NODE_ENV || 'DEV';
 router.use((req, res, next) => {
-	if (envmode === 'DEV') {
-		console.info(`WebActions: Sevicing ${req.url}`);
-		next();
-	}
+  if (envmode === 'DEV') {
+    console.info(`WebActions: Sevicing ${req.url}`);
+    next();
+  }
 });
 
 // Configure cors options
 const corsOptions = {
-	origin: [
+  origin: [
 
-		// Add all hosts for which requests would be accepted.
-		'http://www.crazydeveloper.fail', // HTTP for crazydeveloper host - TODO: remove after moving to GCP
-		'http://rmishra.me', // HTTP endpoint for rmishra.me - TODO: remove after moving to GCP
-		'https://www.crazydeveloper.fail', // crazydeveloper host
-		'https://rmishra.me', // Personal Website
-		'https://www.rmishra.me' // Alt path
-	],
-	optionsSuccessStatus: 200
+    // Add all hosts for which requests would be accepted.
+    'http://www.crazydeveloper.fail', // HTTP for crazydeveloper host - TODO: remove after moving to GCP
+    'http://rmishra.me', // HTTP endpoint for rmishra.me - TODO: remove after moving to GCP
+    'https://www.crazydeveloper.fail', // crazydeveloper host
+    'https://rmishra.me', // Personal Website
+    'https://www.rmishra.me' // Alt path
+  ],
+  optionsSuccessStatus: 200
 };
 
 // Add cors to preflight / OPTIONS
@@ -32,54 +32,29 @@ router.options('*', cors(corsOptions));
 
 // Service reach by email requests for rmishra.me
 router.post('/rme_cb_act_email', cors(corsOptions), async (req, res) => {
-	// Get info from request into vars.
-	let name = req.body.name;
-	let email = req.body.mail;
+  // Get info from request into vars.
+  let name = req.body.name;
+  let email = req.body.mail;
 
-	if (envmode === 'DEV') {
-		console.log(`Processing request for ${name} at ${email}`);
-	}
+  if (envmode === 'DEV') {
+    console.log(`Processing request for ${name} at ${email}`);
+  }
 
-	let user = null;
+  let user = null;
 
-	try {
-		user = await userModel.create({
-			name: name,
-			email: email
-		});
-	} catch (e) {
-		console.message('=== A fault occurred ===');
-		console.error(e.message);
-	}
+  try {
+    user = await userModel.create({
+      name: name,
+      email: email
+    });
+  } catch (e) {
+    console.message('=== A fault occurred ===');
+    console.error(e.message);
+  }
 
-	// TODO: Send email verification
-	// const DOMAIN = process.env.DOMAIN;
-	// const mg = mailgun({
-	// 	apiKey: process.env.MG_API_KEY,
-	// 	domain: DOMAIN
-	// });
-	// const data = {
-	// 	from: process.env.FROM_TEXT || 'email verification',
-	// 	to: email,
-	// 	subject: `Hi, ${name} Lets Talk!`,
-	// 	template: 'contact_verification',
-	// 	'h:X-Mailgun-Variables': {
-	// 		email_base64: Buffer.from(email, 'utf8').toString('base64'),
-	// 		unique_token: user.verification_token
-	// 	}
-	// };
-	// try {
-	// 	mg.messages().send(data, function (error, body) {
-	// 		if (envmode === 'DEV') {
-	// 			console.log(body);
-	// 		}
-	// 	});
-	// } catch (e) {
-	// 	console.message('=== Couldn\'t send email ===');
-	// 	console.log(e);
-	// }
+  // User.verify(onSuccess, onFail)
 
-	res.send('Received! ' + user.name);
+  res.send('Received! ' + user.name);
 });
 
 module.exports = router;
